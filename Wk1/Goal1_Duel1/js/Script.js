959422a1b94c532e910896282dfd94a2681e7cb5
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-11-01 16:36:51 -0400

    Setup Variables and Basic Functions
    Setup Variables and Basic Functions

--- Wk1/Goal1_Duel1/js/Script.js
+++ Wk1/Goal1_Duel1/js/Script.js
@@ -1,7 +1,44 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Jacob R. Minotti
+Nov. 1st, 2014
+Wk1. Assignment
  */
 
+/*
+ * Just testing files
+alert ("Derp");
+*/
+
+(function(){
+    console.log ("Fight!");
+    //naming the players
+   var playerOne = prompt("Player One please type in your name.");
+   var playerTwo = prompt("Player Two please type in your name.");
+   
+   //player damage
+   var PlayerOneDamage = 20;
+   var PlayerTwoDamage = 20;
+   
+   //player health
+   var PlayerOneHealth = 100;
+   var PlayerTwoHealth = 100;
+   
+   //Rounds
+   var round = 1;
+   
+   function fight(){
+       console.log("In the Fight Function.");
+   };
+   
+   function winnerCheck(){
+       
+   };
+
+    /* Starting Program Here */
+fight();
+    
+}
+
+
+)();
 
