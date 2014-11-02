61c14b9c35b30cce6c81d08c7bd84e204cb42569
Noodlecup <jacobminotti@Jacobs-MacBook-Pro.local>
2014-11-01 20:03:04 -0400

    Added Pseudocode to my JavaScript file
    Pseudocode

--- Wk1/Goal1_Duel1/js/Script.js
+++ Wk1/Goal1_Duel1/js/Script.js
@@ -11,15 +11,15 @@ alert ("Derp");
 
 (function(){
     console.log ("Fight!");
-    //naming the players
-   var playerOne = prompt("Player One please type in your name.");
-   var playerTwo = prompt("Player Two please type in your name.");
+    //Players are prompted for a username of their choice.
+   var playerOne = prompt("Player One please type in your username.");
+   var playerTwo = prompt("Player Two please type in your username.");
    
    //player damage
    var playerOneDamage = 20;
    var playerTwoDamage = 20;
    
-   //player health
+   //player's max health at start of the game
    var playerOneHealth = 100;
    var playerTwoHealth = 100;
    
@@ -28,12 +28,12 @@ alert ("Derp");
    
    function fight(){
        console.log("In the Fight Function.");
-       
+       //tells the player information about each other and what round the game is on.
        alert(playerOne+":"+playerOneHealth+" *START* "+playerTwo+":"+playerTwoHealth);
        
        for (var i=0; i < 10; i++){
            
-           // Damage Generating
+           // Computer generates a number inbetween the minimum and maximum damage a player can take for each player
            var minDamageOne = playerOneDamage * .5;
            var minDamageTwo = playerTwoDamage * .5;
            var f1 = Math.floor(Math.random()*(playerOneDamage-minDamageOne)+minDamageOne);
@@ -42,7 +42,7 @@ alert ("Derp");
            //console.log(f1);
            //console.log(f2);
            
-           //Dealing Damaging
+           //Players get dealt damage based off of the numbers that the computer has generated for them.
            playerOneHealth-=f1;
            playerTwoHealth-=f2;
            
@@ -51,7 +51,7 @@ alert ("Derp");
           var results = winnerCheck();
           console.log(results);
           
-          // Looking for winner and breaking program
+          // Looking for winner and stop the program when a winner can be found or if there is no winner after the end of the ten rounds. 
           if (results === "No Winner"){
               round++;
               alert(playerOne+":"+playerOneHealth+" *ROUND "+round+" OVER*"+playerTwo+":"+playerTwoHealth);
@@ -62,7 +62,10 @@ alert ("Derp");
            
        };
    };
-   
+   /*Look for a winner if there isn't a winner ignore it until the end of the tenth round.
+    * If Player one's & two's Health both reach =>0 then tell the players they both died.
+    * If either player one's or player two's health hit zero or below then print out the victor of the game.
+   */
    function winnerCheck(){
        console.log("In Winner check function");
        var result = "No Winner";
